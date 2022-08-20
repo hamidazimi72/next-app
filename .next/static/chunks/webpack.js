8756a6d0fe7e0d5143893ef844cf9d583939bf8e
hamid1372 <hamidazimi1372@gmail.com>
2022-08-20 17:59:35 +0430

    Initial Commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "13df6a6eb4b2c10a"; }
+/******/ 		__webpack_require__.h = function() { return "be97f550fa550d26"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
